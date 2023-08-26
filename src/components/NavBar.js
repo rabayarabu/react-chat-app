a662c4435d065879d02d1fe94220b52a36c28fda
rabayarabu <rabu9149@gmail.com>
2023-08-26 15:31:48 +0600

    Change sign in out button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
@@ -21,13 +20,11 @@ const NavBar = () => {
           Sign Out
         </button>
       ) : (
-        <button className="sign-in">
-          <img
+        <button className="sign-in"
             onClick={googleSignIn}
-            src={GoogleSignin}
             alt="sign in with google"
-            type="button"
-          />
+            type="button">
+              Sign in
         </button>
       )}
     </nav>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
-import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase";
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import logo from '../img/logo.png';
 
 const Welcome = () => {
   const googleSignIn = () => {
@@ -12,16 +12,14 @@ const Welcome = () => {
   return (
     <main className="welcome">
       <h2>Welcome to React Chat.</h2>
-      <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
-      <p>Sign in with Google to chat with with your fellow React Developers.</p>
-      <button className="sign-in">
-        <img
-          onClick={googleSignIn}
-          src={GoogleSignin}
-          alt="sign in with google"
-          type="button"
-        />
-      </button>
+      <img src={logo} alt="logo" width={50} height={50} />
+      <p>Sign in with Google to chat with with your ....</p>
+      <button className="sign-in"
+            onClick={googleSignIn}
+            alt="sign in with google"
+            type="button">
+              Sign in
+        </button>
     </main>
   );
 };
